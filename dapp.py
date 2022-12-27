c89ac4ef80fec05b85f44984fe38bcbafc8cf967
Camilo Molano <camilo@algorand.foundation>
2022-12-27 11:54:30 -0500

    Gobla storage demo

--- dapp.py
+++ dapp.py
@@ -2,6 +2,16 @@ from beaker import *
 from pyteal import *
 
 class Bootcamp(Application):
+
+    name = ApplicationStateValue(stack_type=TealType.bytes)
+
+    @external
+    def set_name(self, name:abi.String):
+        return self.name.set(name.get())
+    
+    @external
+    def get_name(self, *, output: abi.String):
+        return output.set(self.name.get())
     
     @create
     def create(self):
@@ -26,6 +36,19 @@ class Bootcamp(Application):
             [option.get() == Bytes("B"), output.set(Bytes("B was selected"))]
         )
 
+    @external
+    def seq_expression(self, option: abi.String, *, output:abi.String):
+        if_variable = If(Int(1) > Int(0), output.set(Bytes("True")), output.set(Bytes("False")))
+
+        return Seq(
+            if_variable,
+            Cond(
+                [option.get() == Bytes("A"), output.set(Bytes("A was selected"))],
+                [option.get() == Bytes("B"), output.set(Bytes("B was selected"))]
+            )
+        )
+    
+
 app = Bootcamp(version=8)
 app.dump()
 
@@ -44,3 +67,6 @@ print(app_client.call(Bootcamp.hello_name, name="Camilo").return_value)
 print(app_client.call(Bootcamp.hello_sender).return_value)
 print(app_client.call(Bootcamp.if_condition).return_value)
 print(app_client.call(Bootcamp.cond_expression, option="A").return_value)
+print(app_client.call(Bootcamp.seq_expression, option="A").return_value)
+app_client.call(Bootcamp.set_name, name="Alejandro").return_value
+print(app_client.call(Bootcamp.get_name).return_value)
